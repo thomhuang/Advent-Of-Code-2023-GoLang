83c9495f178ea1fc8cf1411b80e9952ddaa2d0d4
thomas huang <thomaskhuang@g.ucla.edu>
2024-12-04 08:59:19 -0800

    day 3 pt 2 + cleaup

--- Day 3/day_3.go
+++ Day 3/day_3.go
@@ -18,29 +18,39 @@ func main() {
 	sc := bufio.NewScanner(file)
 
 	total := 0
+	instructedTotal := 0
+	do := true
 	for sc.Scan() {
 		text := sc.Text()
 		for i := 0; i < len(text); i++ {
-			if text[i] == 'm' && text[i+1] == 'u' && text[i+2] == 'l' && text[i+3] == '(' {
+			if i+4 < len(text) && text[i] == 'm' && text[i+1] == 'u' && text[i+2] == 'l' && text[i+3] == '(' {
 				i += 4
 				first := parseNumber(text, &i)
-				if i+2 < len(text) {
+				if i+1 < len(text) {
 					if text[i] == ',' {
-						i += 1
+						i++
 						second := parseNumber(text, &i)
 						if i < len(text) && text[i] == ')' {
 							if first != 0 && second != 0 {
 								total += first * second
 							}
+							if do && first != 0 && second != 0 {
+								instructedTotal += first * second
+							}
 						}
 					}
 				}
-
+			} else if i+1 < len(text) && text[i] == 'd' && text[i+1] == 'o' {
+				if i+2 < len(text) {
+					i += 2
+					parseInstruction(text, &i, &do)
+				}
 			}
 		}
 	}
 
 	fmt.Println(total)
+	fmt.Println(instructedTotal)
 }
 
 func parseNumber(text string, i *int) int {
@@ -55,4 +65,15 @@ func parseNumber(text string, i *int) int {
 	}
 
 	return 0
+}
+
+func parseInstruction(text string, i *int, prevInstruction *bool) {
+	if *i+1 < len(text) && text[*i] == '(' && text[*i+1] == ')' {
+		*i += 1
+		*prevInstruction = true
+		return
+	} else if *i+4 < len(text) && text[*i] == 'n' && text[*i+1] == '\'' && text[*i+2] == 't' && text[*i+3] == '(' && text[*i+4] == ')' {
+		*i += 4
+		*prevInstruction = false
+	}
 }
